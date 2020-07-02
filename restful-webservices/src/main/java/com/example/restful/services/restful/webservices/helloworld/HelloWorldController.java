5f2a35539a5948fe6d9397cc04fc26fd793a2a25
Dell <Dell@DESKTOP-PF8DQE0>
2020-07-02 11:03:59 +0530

    first commit to restful webserices project

---
+++ restful-webservices/src/main/java/com/example/restful/services/restful/webservices/helloworld/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.example.restful.services.restful.webservices.helloworld;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@Autowired
+	private MessageSource messageSource;
+
+	@GetMapping(path = "/hello-world")
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String helloworld() {
+		return "Hello world";
+	}
+
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloworldBean() {
+		return new HelloWorldBean("Hello world");
+	}
+
+	@GetMapping(path = "/hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloworldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world,%s", name));
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+	}
+
+}
